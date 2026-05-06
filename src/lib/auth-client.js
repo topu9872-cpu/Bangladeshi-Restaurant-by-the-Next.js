cd8c5b917cdc0cb28a08790720bceec4234e71d1
topu9872-cpu <topu9872@gmail.com>
2026-05-06 19:53:22 +0600

    fixed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,9 +1,9 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
    
-    // baseURL:process.env.BETTER_AUTH_URL 
+    baseURL:process.env.BETTER_AUTH_URL 
     
-    baseURL:'http://localhost:3000'
+    // baseURL:'http://localhost:3000'
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
